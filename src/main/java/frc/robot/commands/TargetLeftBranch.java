8ea81cc43de6039973c5f074ee560ec6e1cc871a
McD4505 <econn@mcdonogh.org>
2025-01-30 20:22:07 -0500

    added command to set target pose (left branch)

---
+++ src/main/java/frc/robot/commands/TargetLeftBranch.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Vision;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class TargetLeftBranch extends Command {
+  /** Creates a new TargetLeftBranch. */
+  private CommandSwerveDrivetrain drivetrain;
+
+  public TargetLeftBranch(CommandSwerveDrivetrain drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.drivetrain = drivetrain;
+    addRequirements(drivetrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Pose2d tagPose = Vision.getTargetPose();
+
+    Translation2d baseTranslation = new Translation2d(0.5, Units.inchesToMeters(-6));
+
+    Transform2d transform = new Transform2d(baseTranslation, Rotation2d.fromDegrees(180));
+
+    Pose2d targetPose = tagPose.plus(transform);
+    drivetrain.setTargetPose(targetPose);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
