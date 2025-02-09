c3018e3e7147e6dae62e8d8503de33a861f6e95a
McD4505 <econn@mcdonogh.org>
2025-02-09 12:28:03 -0500

    added coral dispensing subsystem (untested)

---
+++ src/main/java/frc/robot/subsystems/Dispenser.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Dispenser extends SubsystemBase {
+  private SparkMax max = new SparkMax(9, MotorType.kBrushless);
+
+  private SparkClosedLoopController controller = max.getClosedLoopController();
+
+  private final double wheelRadius = Units.inchesToMeters(2);
+  private final double conversionFactor = 2 * Math.PI * wheelRadius;
+
+  /** Creates a new Dispenser. */
+  public Dispenser() {
+    SparkMaxConfig config = new SparkMaxConfig();
+
+    config
+      .inverted(false)
+      .idleMode(IdleMode.kCoast);
+
+    config.encoder
+      .positionConversionFactor(conversionFactor)
+      .velocityConversionFactor(conversionFactor);
+
+    max.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public void setSpeed(double metersPerSecond) {
+    controller.setReference(metersPerSecond, ControlType.kVelocity);
+  }
+
+  public void stop() {
+    controller.setReference(0, ControlType.kDutyCycle);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
